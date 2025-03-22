35bb23dbb6eab1e71d3f7a97e3c04313efb0af1f
Jacob Gadikian <jacobgadikian@gmail.com>
2025-03-23 01:17:24 +0700

    resolve issue

--- cmd/horsed/cmd/commands.go
+++ cmd/horsed/cmd/commands.go
@@ -19,8 +19,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/rpc"
 	"github.com/cosmos/cosmos-sdk/client/snapshot"
 	"github.com/cosmos/cosmos-sdk/server"
-	"github.com/cosmos/cosmos-sdk/server/api"
-	"github.com/cosmos/cosmos-sdk/server/config"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -203,8 +201,4 @@ var tempDir = func() string {
 	defer os.RemoveAll(dir)
 
 	return dir
-}
-
-func (app *app.App) RegisterAPIRoutes(apiSvr *api.Server, apiConfig config.APIConfig) {
-	// Your existing implementation...
 }
